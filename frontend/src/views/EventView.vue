06020cf28369c1a17a57c62e86b31b85b0419d2f
Tay Yee Teng <67989985+TayYeeTeng@users.noreply.github.com>
2025-03-11 21:45:27 +0800

    update front end skeleton

---
+++ frontend/src/views/EventView.vue
@@ -0,0 +1,24 @@
+<template>
+    <!-- Your HTML content here -->
+</template>
+
+<script>
+export default {
+    name: 'event',
+    data() {
+        return {
+            // Your component data here
+        }
+    },
+    methods: {
+        // Your component methods here
+    },
+    mounted() {
+        // Lifecycle hook: mounted
+    }
+}
+</script>
+
+<style scoped>
+/* Your CSS styles here */
+</style>
---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,24 @@
+<template>
+    <!-- Your HTML content here -->
+</template>
+
+<script>
+export default {
+    name: 'login',
+    data() {
+        return {
+            // Your component data here
+        }
+    },
+    methods: {
+        // Your component methods here
+    },
+    mounted() {
+        // Lifecycle hook: mounted
+    }
+}
+</script>
+
+<style scoped>
+/* Your CSS styles here */
+</style>
