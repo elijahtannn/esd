cd768db49a954530b1d238c1754d94219b163d26
pritikaaaaaaa <pritika.k.2023@scis.smu.edu.sg>
2025-03-15 00:51:22 +0800

    profile layout

--- frontend/src/views/ProfileView.vue
+++ frontend/src/views/ProfileView.vue
@@ -1,6 +1,19 @@
 <template>
     <div>
         <NavBar/>
+
+        <!-- Banner -->
+        <div class="container-fluid bannerImg banner mx-0 p-5">
+            <div class="row">
+                <div class="col-8">
+                <div>
+                    <p class="bannerText">Hello,<br> {{ user.name }}</p>
+                </div>
+                </div>
+                <div class="col-4"></div>
+            </div>
+        </div>
+        
         <div class="container mt-4" v-if="user">
             <h2>Profile</h2>
             <div class="card">
@@ -45,5 +58,14 @@ export default {
 .card {
     max-width: 600px;
     margin: 0 auto;
+}
+
+.bannerImg {
+    background-image: linear-gradient(to right, rgba(0, 0, 0, 0.9), rgba(255, 255, 255, 0.1)),
+        url("../assets/images/background_profile.png");
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 60vh;
+    width: 100%;
 }
 </style>
